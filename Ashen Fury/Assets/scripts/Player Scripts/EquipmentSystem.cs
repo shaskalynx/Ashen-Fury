1b7a15042fe468fbff67b050e52a31501bc2ca6a
shaskalynx <alexdangasison023@gmail.com>
2025-04-05 22:50:14 +0800

    Some minor changes and additions

--- Ashen Fury/Assets/scripts/Player Scripts/EquipmentSystem.cs
+++ Ashen Fury/Assets/scripts/Player Scripts/EquipmentSystem.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+using UnityEngine.UI;
+
 public class EquipmentSystem : MonoBehaviour
 {
+    [Header("Weapon Settings")]
     [SerializeField] GameObject weaponHolder;
     [SerializeField] GameObject[] weapons = new GameObject[3]; // Array of 3 weapons
     [SerializeField] GameObject weaponSheath;
- 
+
+    [Header("UI Elements")]
+    [SerializeField] private Image[] weaponIcons = new Image[3];
+    [SerializeField] private Color selectedColor = new Color(1f, 1f, 1f, 1f);
+    [SerializeField] private Color unselectedColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+
     private int currentWeaponIndex = 0;
     public GameObject currentWeaponInHand;
     GameObject currentWeaponInSheath;
@@ -17,6 +24,7 @@ public class EquipmentSystem : MonoBehaviour
         if (weapons[0] != null)
         {
             currentWeaponInSheath = Instantiate(weapons[0], weaponSheath.transform);
+            UpdateWeaponUI();
         }
     }
 
@@ -44,6 +52,19 @@ public class EquipmentSystem : MonoBehaviour
             // If weapon is sheathed, switch in sheath
             Destroy(currentWeaponInSheath);
             currentWeaponInSheath = Instantiate(weapons[currentWeaponIndex], weaponSheath.transform);
+        }
+
+        UpdateWeaponUI();
+    }
+
+    private void UpdateWeaponUI()
+    {
+        for (int i = 0; i < weaponIcons.Length; i++)
+        {
+            if (weaponIcons[i] != null)
+            {
+                weaponIcons[i].color = (i == currentWeaponIndex) ? selectedColor : unselectedColor;
+            }
         }
     }
 
